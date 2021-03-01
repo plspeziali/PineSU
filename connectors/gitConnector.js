fcfe33c88925244dc0f943cfff690b8ab796a7e5
Paolo Speziali <speziali99@gmail.com>
2021-03-01 18:46:43 +0100

    First part of business logic

--- connectors/gitConnector.js
+++ connectors/gitConnector.js
@@ -2,60 +2,63 @@ const git = simpleGit(".");
 var num;
 var fileslist;
 
-function init(){
-    git.checkIsRepo()
-        .then(isRepo => !isRepo && git.init())
-        .then(() => git.fetch())
-        .then(() => git.raw('rev-list','--all','--count',(err,log) => num = log))
-}
 
-function add(_arg){
-    git.add(_arg);
-}
-
-function addAll(){
-    git.add('./*')
-}
-
-function commit(_msg,_enmsg){
-    if(_enmsg){
-        git.commit(_msg)
-            .raw('rev-list','--all','--count',(err,log) => num = log);
-    } else {
-        git.commit("commit #" + num + " by PineSU")
-            .raw('rev-list','--all','--count',(err,log) => num = log);
+class GitConnector{
+    init(){
+        git.checkIsRepo()
+            .then(isRepo => !isRepo && git.init())
+            .then(() => git.fetch())
+            .then(() => git.raw('rev-list','--all','--count',(err,log) => num = log))
     }
-}
 
-async function getRepoFiles(){
-    try{
-        return await git.raw('ls-tree','--full-tree','-r','--name-only','HEAD',(err, log) => {return log.split("\n")});
-    } catch (e) {
-        // TODO
+    add(_arg){
+        git.add(_arg);
     }
-}
 
-function addRemote(_user,_pass,_repo){
-    git().silent(true)
-        .clone(`https://${_user}:${_pass}@${_repo}`)
-        .then(() => console.log('finished'))
-        .catch((err) => console.error('failed: ', err));
-}
+    add(){
+        git.add('./*')
+    }
 
-function push(){
-    git.push('origin', 'master')
-}
-
-function pull(){
-    git.pull();
-}
-
-function custom(_commands){
-    git(path).raw(_commands, (err, result) => {
-        if(err){
-            console.error(err);
+    commit(_msg,_enmsg){
+        if(_enmsg){
+            git.commit(_msg)
+                .raw('rev-list','--all','--count',(err,log) => num = log);
         } else {
-            console.log(result);
+            git.commit("commit #" + num + " by PineSU")
+                .raw('rev-list','--all','--count',(err,log) => num = log);
         }
-    });
+    }
+
+    async getRepoFiles(){
+        try{
+            return await git.raw('ls-tree','--full-tree','-r','--name-only','HEAD',(err, log) => {return log.split("\n")});
+        } catch (e) {
+            // TODO
+        }
+    }
+
+    addRemote(_user,_pass,_repo){
+        git().silent(true)
+            .clone(`https://${_user}:${_pass}@${_repo}`)
+            .then(() => console.log('finished'))
+            .catch((err) => console.error('failed: ', err));
+    }
+
+    push(){
+        git.push('origin', 'master')
+    }
+
+    pull(){
+        git.pull();
+    }
+
+    custom(_commands){
+        git(path).raw(_commands, (err, result) => {
+            if(err){
+                console.error(err);
+            } else {
+                console.log(result);
+            }
+        });
+    }
 }
--- logic/businessLogic.js
+++ logic/businessLogic.js
@@ -1,26 +1,52 @@
 const fs = require('fs');
-var merkle = require('./lib/merkle.js');
 var mkdirp = require('mkdirp');
-var git = require('./connectors/gitConnector.js');
+var git = new GitConnector();
+var treelist = new TreeList();
 const rl = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+
+
 function init(){
     git.init();
-    mkdirp('./.pinesu', function(err) {});
+    mkdirp(__dirname+'\\.pinesu', function(err) {});
+}
+
+function addFileSU(_file){
+    git.add(_file);
+}
+
+function addAllSU(){
+    git.add();
 }
 
 function commitSU(){
-
+    git.commit("",false);
 }
 
-function pushSU(){
-
+function commitSU(_msg){
+    git.commit(_msg,true);
 }
-
 
 function calculateSU(){
-
+    var list;
+    var tree;
+    git.getRepoFiles((res) => list = res)
+        .then(tree = treelist.createHashLists(list))
+        .then(saveJSON(tree));
 }
+
+function saveJSON(_tree){
+    const jsonContent = JSON.stringify(_tree);
+ 
+    fs.writeFile(__dirname+"\\.pinesu\\filetree.json", jsonContent, 'utf8', function (err) {
+       if (err) {
+          return console.log(err);
+       }
+ 
+       console.log("The file was saved!");
+       return;
+    }); 
+ }
