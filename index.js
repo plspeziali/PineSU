d674c62501c9298dc92186193382008a14030194
Paolo <speziali99@gmail.com>
2021-03-12 22:49:46 +0100

    Migliorata gestione CLI

--- index.js
+++ index.js
@@ -22,18 +22,77 @@ const run = async () => {
   if(inqstart.startans === "Exit"){
     console.log(chalk.green("Goodbye!"));
     process.exit(0);
+  } else if (inqstart.startans === "Create new SU") {
+
+    await create();
+
   } else if (inqstart.startans === "Download SU"){
     // TODO
   } else if (inqstart.startans === "Export SU"){
     // TODO
   } else if (inqstart.startans === "Get / Change identity"){
-    var res = files.readID();
+    
+    await identity();
+
+  }
+
+};
+
+const create = async () => {
+
+  if (files.directoryExists('.git')) {
+    console.log(chalk.green('Already a Git repository!'));
+  } else {
+    const setup = await inquirer.gitSetup();
+    if(setup.gitinit == "Yes"){
+      gitLogic.init();
+    }else{
+      console.log('PineSU requires the folder to be initialized as a git repository in order to\nwork');
+      process.exit();
+    }
+  }
+
+  /*if(!(await gitLogic.hasRemote())){
+    await gitLogic.addRemoteSU("http://localhost:7005/prova");
+  }*/
+
+  const inqignore = await inquirer.gitAdd();
+  if(inqignore.gitignore == "Yes"){
+    await files.createGitignore();
+  }
+
+  gitLogic.addFileSU('.gitignore');
+  gitLogic.addAllSU();
+  files.createPineSUDir();
+
+  gitLogic.commitSU("").then(() => {gitLogic.calculateSU()});
+  const details = await inquirer.askSUDetails(files.getCurrentDirectoryBase());
+  await files.saveJSON(details,"suinfo");
+  console.log(chalk.green("The Storage Unit has been created!"));
+
+  gitLogic.resetCommit();
+  gitLogic.addAllSU();
+
+  const commit = await inquirer.gitCommit();
+  if(commit.message != "[Commit # by PineSU]"){
+    await gitLogic.commitSU(commit.message);
+  } else {
+    await gitLogic.commitSU("");
+  }
+  
+  //console.log(chalk.yellow("The Storage Unit will now be uploaded to our server..."));
+  //await gitLogic.pushSU();
+};
+
+const identity = async () => {
+
+  var res = files.readID();
     if(res.id === "null"){
       const inquser = await inquirer.chooseUsername();
       files.writeID(inquser.username);
       var res = files.readID();
     }
-    console.log("Your username is "+res.username+" and your ID is "+res.id);
+    console.log("Your username is "+chalk.black.bgGreen(res.username)+" and your ID is "+chalk.black.bgYellow(res.id));
     
     const inqchuser = await inquirer.changeUsername();
 
@@ -44,54 +103,6 @@ const run = async () => {
     }
 
     run();
-
-  } else if (inqstart.startans === "Create new SU") {
-
-    if (files.directoryExists('.git')) {
-      console.log(chalk.green('Already a Git repository!'));
-    } else {
-      const setup = await inquirer.gitSetup();
-      if(setup.gitinit == "Yes"){
-        gitLogic.init();
-      }else{
-        console.log('PineSU requires the folder to be initialized as a git repository in order to\nwork');
-        process.exit();
-      }
-    }
-  
-    /*if(!(await gitLogic.hasRemote())){
-      await gitLogic.addRemoteSU("http://localhost:7005/prova");
-    }*/
-  
-    const inqignore = await inquirer.gitAdd();
-    if(inqignore.gitignore == "Yes"){
-      await files.createGitignore();
-    }
-  
-    gitLogic.addFileSU('.gitignore');
-    gitLogic.addAllSU();
-    files.createPineSUDir();
-  
-    gitLogic.commitSU("").then(() => {gitLogic.calculateSU()});
-    const details = await inquirer.askSUDetails(files.getCurrentDirectoryBase());
-    await files.saveJSON(details,"suinfo");
-    console.log(chalk.green("The Storage Unit has been created!"));
-  
-    gitLogic.resetCommit();
-    gitLogic.addAllSU();
-  
-    const commit = await inquirer.gitCommit();
-    if(commit.message != "[Commit # by PineSU]"){
-      await gitLogic.commitSU(commit.message);
-    } else {
-      await gitLogic.commitSU("");
-    }
-    
-    //console.log(chalk.yellow("The Storage Unit will now be uploaded to our server..."));
-    //await gitLogic.pushSU();
-
-  }
-
 };
-  
+
 run();
