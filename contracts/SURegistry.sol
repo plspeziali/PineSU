2912b117ea5f629c7005965dbf464e5e8202cfa9
Paolo Speziali <speziali99@gmail.com>
2021-03-30 13:01:56 +0200

    contratto completo, da testare e rifinire

--- contracts/SURegistry.sol
+++ contracts/SURegistry.sol
@@ -19,6 +19,19 @@ contract SURegistry {
     } 
     
     function verify(bytes _name, bytes32 _owner) public view returns (boolean) {
-        return documents[hash];
+        // We know the length of the array
+        uint memory arrayLength = registry.length;
+        uint8 memory result = 0; // 0: nothing found; 1: found name, not name with that owner; 2: all found
+        
+        for (uint memory i=0; i<arrayLength; i++) {
+            if(registry[i].name == _name){
+                if(registry[i].owner == _owner){
+                    return 2;
+                }
+                result = 1;
+            }
+        }
+
+        return result;
     }
 }
